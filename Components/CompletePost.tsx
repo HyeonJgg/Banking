19950fa46c2d8655bc7340d4f6b20e8118b8e07f
Hyjiii <wjsguswl310@naver.com>
2022-06-12 18:28:39 +0900

    변수 변경

--- Components/CompletePost.tsx
+++ Components/CompletePost.tsx
@@ -1,18 +1,17 @@
 // import 'react-native-gesture-handler';
 import React from 'react';
 import { StyleSheet,View,ScrollView,Text,Button } from 'react-native';
-import { money, getname } from './Remit';
+import { money, comgetname } from './Remit';
 
 const Color = {
   purple : "#dda0dd",
 }
 
 function CompletePost({navigation}: {navigation: any}) {
-
   return (
     <ScrollView style={styles.container}>
       <View style = {styles.marginBottom}>
-        <Text style={styles.textbold}>{getname}님에게 {money}원을 보냈습니다.</Text>
+        <Text style={styles.textbold}>{comgetname}님에게 {money}원을 보냈습니다.</Text>
       </View>
       <View style = {styles.marginTop}>
         <Button
@@ -25,7 +24,7 @@ function CompletePost({navigation}: {navigation: any}) {
       </View>      
     </ScrollView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container :{
